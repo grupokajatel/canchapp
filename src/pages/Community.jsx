0942e59affa5b1d2ecdee46346063b76a5fd2352
Grupo Kajatel <grupokajatel001@gmail.com>
2025-12-03 03:29:12 +0000

    File changes

--- src/pages/Community.jsx
+++ src/pages/Community.jsx
@@ -5,7 +5,7 @@ import { createPageUrl } from "@/utils";
 import { format } from "date-fns";
 import { es } from "date-fns/locale";
 import {
-  Plus, Trophy, Users, Search, Filter, Calendar, MapPin, Zap, Upload, Image
+  Plus, Trophy, Users, Search, Filter, Calendar, MapPin, Zap, Upload, Image, X
 } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
